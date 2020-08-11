0ece12a2cc9c3add5198d85f74d0bb2c5bee094a
Souarv Sharma <souravgopal25@gmail.com>
2020-08-11 15:05:38 +0530

    Variable Decleration and interpollation

--- bin/helloflutter.dart
+++ bin/helloflutter.dart
@@ -1,4 +1,39 @@
 void main() {
   print('Hello, Dart!');
+  //declaring a variable
+  //DATA type
+  /*
+  Data type
+    * Numbers
+        =>int
+        =>double
+    * strings
+    *Boolean
+    *lists
+    *runes
+    *symbols
+   */
+  int age=10;
+  double age1=6.5;
+  var x=2.5;
+  var y=90;
+  bool isTrue=true;
+  String name="Sourav";
+  var name1="sourav";
+
+
+  print(age);
+  print(age1);
+  print(x+y);
+
+
+  //STRING INTERPOLATION
+  //Use of $ instead of +
+  String s1="It's Easy";
+  print("My name is $name1");
+  print("Value of x=$x");
+  print("$name = $x ");
+
+
 
 }

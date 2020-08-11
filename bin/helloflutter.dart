3984bb23b78d3a63bdbfb034d03e46352656f205
Souarv Sharma <souravgopal25@gmail.com>
2020-08-11 17:41:57 +0530

    Callable Class Implemented

--- bin/helloflutter.dart
+++ bin/helloflutter.dart
@@ -1,38 +1,16 @@
 import 'model.dart';
 
 void main() {
- /*
-             LIST are of 2 types
+  var perosnOne=Person();
+  perosnOne(20,"Sourav");
 
-             * Fixed    List
-             * Growable List
+}
 
-
-         in fixed we pass length and in growable we dont pass parameter
-   */
-
-   Student student=new Student(1, "Sourav");
-   Student student1=new Student(2, "RAM");
-   Student student2=new Student(3, "Shyam");
-
-
- List<Student> mlist=new List();
-  mlist.add(student);
-  mlist.add(student1);
-  mlist.add(student2);
-  int i;
-  for(i=0;i<mlist.length;i++){
-   //instead of .get() we use .elementAt()
-   var obj=mlist.elementAt(i);
-   print(obj.name);
+class Person{
+  //WE have implemented callable person
+  call(int age,String name){
+    print("The name of the person is ${name} and age is ${age}");
 
   }
-
-  //in java we used for each loop
-   // in dart we use this8
-  for(Student student in mlist){
-    print(student.name);
-  }
-
 
 }

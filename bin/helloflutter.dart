b9d19206adc866887c1c0301eca7dcd082c5d7fc
Souarv Sharma <souravgopal25@gmail.com>
2020-08-11 15:54:34 +0530

    Functions optional named Parameters

--- bin/helloflutter.dart
+++ bin/helloflutter.dart
@@ -1,16 +1,17 @@
 void main() {
 
-int i;
-for(i=0;i<10;i++){
-  print(i);
+print("Area is  :${findArea(10, 20)}");
+print("Perimeter is:${findPerimeter(10, 20)}");
+sqr(20);
+print(findVolume(2,height: 10,breadth: 5));
 }
-
-while(i!=0){
-  print(i);
-  i--;
-  if(i==5){
-    break;
-  }
+void sqr(int x) => print(x*x);
+double findArea(double length,double breadth){
+  return length*breadth;
 }
-
+double findPerimeter(double l,double b){
+  return 2*(l+b);
+}
+int findVolume(int length,{int breadth,int height}){
+  return length*breadth*height;
 }
